0d93109ec2a49cda975228bfe6c9336dbde5ab56
cdnkvdbjjkdbnkzsdsdkdhsdsjdshod <41491967+cdnkvdbjjkdbnkzsdsdkdhsdsjdshod@users.noreply.github.com>
2018-07-26 22:59:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -405,77 +405,94 @@ $invite - معلومات عن الدعوة :soccer:
 }
 });
 
+const moment = require('moment')
 client.on('message',async message => {
-var room;
-var title;
-var duration;
-var gMembers;
-var filter = m => m.author.id === message.author.id;
-if(message.content.startsWith(prefix + "giveaway")) {
-  //return message.channel.send(':heavy_multiplication_x:| **هذا الامر معطل حاليا.. ``حاول في وقت لاحق``**');
- if(!message.guild.member(message.author).hasPermission('MANAGE_GUILD')) return message.channel.send(':heavy_multiplication_x:| **يجب أن يكون لديك خاصية التعديل على السيرفر**');
- message.channel.send(`:eight_pointed_black_star:| **منشن الروم الذي تريد به القيف اواي**`).then(msg => {
-   message.channel.awaitMessages(filter, {
-     max: 1,
-     time: 20000,
-     errors: ['time']
-   }).then(collected => {
-     let room = message.guild.channels.find('name', collected.first().content);
-     if(!room) return message.channel.send(':heavy_multiplication_x:| **لم اقدر على ايجاد الروم المطلوب**');
-     room = collected.first().content;
-     collected.first().delete();
-     msg.edit(':eight_pointed_black_star:| **اكتب مدة القيف اواي**').then(msg => {
-       message.channel.awaitMessages(filter, {
-         max: 1,
-         time: 20000,
-         errors: ['time']
-       }).then(collected => {
-         if(isNaN(collected.first().content)) return message.channel.send(':heavy_multiplication_x:| **يجب عليك ان تحدد وقت زمني صحيح.. ``يجب عليك اعادة كتابة الامر``**');
-         if(collected.first().content > 180) return message.channel.send(':heavy_multiplication_x:| **الوقت لا يزيد عن 180 دقيقة .. `يتم حساب الوقت بالدقائق ان كنت لا تعلم`**');
-         duration = collected.first().content * 60000;
-         collected.first().delete();
-         msg.edit(':eight_pointed_black_star:| **واخيرا اكتب على ماذا تريد القيف اواي**').then(msg => {
-           message.channel.awaitMessages(filter, {
-             max: 1,
-             time: 20000,
-             errors: ['time']
-           }).then(collected => {
-             title = collected.first().content;
-             collected.first().delete();
-             try {
-               let giveEmbed = new Discord.RichEmbed()
-               .setAuthor(message.guild.name, message.guild.iconURL)
-               .setTitle(title)
-               .setDescription(`المدة : ${duration / 60000} دقائق`)
-               .setFooter(message.author.username, message.author.avatarURL);
-               message.guild.channels.find('name', room).send(giveEmbed).then(m => {
-                  let re = m.react('✨');
-                  setTimeout(() => {
-                    let users = m.reactions.get("✨").users;
-                    let list = users.array().filter(u => u.id !== m.author.id);
-                    let gFilter = list[Math.floor(Math.random() * list.length) + 1]
-                    let endEmbed = new Discord.RichEmbed()
-                    .setAuthor(message.author.username, message.author.avatarURL)
-                    .setTitle(title)
-                    .addField('انتهى القيف اواي !',`الفائز هو : ${gFilter}`)
-                    .setFooter(message.guild.name, message.guild.iconURL);
-                    m.edit(endEmbed);
-                  },duration);
-                });
-               msg.edit(`:heavy_check_mark:| **تم اعداد القيف اواي**`);
-             } catch(e) {
-               msg.edit(`:heavy_multiplication_x:| **لم اقدر على اعداد القيف اواي بسبب نقص الخصائص**`);
-               console.log(e);
-             }
-           });
-         });
-       });
-     });
-   });
- });
+  var time = moment().format('Do MMMM YYYY , hh:mm');
+  var room;
+  var title;
+  var duration;
+  var gMembers;
+  var currentTime = new Date(),
+hours = currentTime.getHours() + 3 ,
+minutes = currentTime.getMinutes(),
+done = currentTime.getMinutes() + duration / 60000 ,
+seconds = currentTime.getSeconds();
+if (minutes < 10) {
+minutes = "0" + minutes;
 }
-});
+var suffix = "AM";
+if (hours >= 12) {
+suffix = "PM";
+hours = hours - 12;
+}
+if (hours == 0) {
+hours = 12;
+}
 
+  var filter = m => m.author.id === message.author.id;
+  if(message.content.startsWith(prefix + "$giveaway")) {
+     //return message.channel.send(':heavy_multiplication_x:| **هذا الامر معطل حاليا.. ``حاول في وقت لاحق``**');
+    if(!message.guild.member(message.author).hasPermission('MANAGE_GUILD')) return message.channel.send(':heavy_multiplication_x:| **يجب أن يكون لديك خاصية التعديل على السيرفر**');
+    message.channel.send(`:eight_pointed_black_star:| **Send ID channel For the Giveaway**`).then(msg => {
+      message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 20000,
+        errors: ['time']
+      }).then(collected => {
+        let room = message.guild.channels.get(collected.first().content);
+        if(!room) return message.channel.send(':heavy_multiplication_x:| **i Found It :(**');
+        room = collected.first().content;
+        collected.first().delete();
+        msg.edit(':eight_pointed_black_star:| **Time For The Giveaway**').then(msg => {
+          message.channel.awaitMessages(filter, {
+            max: 1,
+            time: 20000,
+            errors: ['time']
+          }).then(collected => {
+            if(isNaN(collected.first().content)) return message.channel.send(':heavy_multiplication_x:| **The Time Be Nambers `` Do the Commend Agin``**');
+            duration = collected.first().content * 60000;
+            collected.first().delete();
+            msg.edit(':eight_pointed_black_star:| **Now send The Present **').then(msg => {
+              message.channel.awaitMessages(filter, {
+                max: 1,
+                time: 20000,
+                errors: ['time']
+              }).then(collected => {
+                title = collected.first().content;
+                collected.first().delete();
+                msg.delete();
+                message.delete();
+                try {
+                  let giveEmbed = new Discord.RichEmbed()
+                  .setDescription(`**${title}** \nReact Whit 🎉 To Enter! \n**Time remaining: Minutes :${duration / 60000}**\n **Created at :** ${hours}:${minutes}:${seconds} ${suffix}`)
+                  .setFooter(message.author.username, message.author.avatarURL);
+                  message.guild.channels.get(room).send(' :heavy_check_mark: **Giveaway Created** :heavy_check_mark:' , {embed: giveEmbed}).then(m => {
+                     let re = m.react('🎉');
+                     setTimeout(() => {
+                       let users = m.reactions.get("🎉").users;
+                       let list = users.array().filter(u => u.id !== m.author.id !== client.user.id);
+                       let gFilter = list[Math.floor(Math.random() * list.length) + 0]
+                       let endEmbed = new Discord.RichEmbed()
+                       .setAuthor(message.author.username, message.author.avatarURL)
+                       .setTitle(title)
+                       .addField('Giveaway End !🎉',`Winners : ${gFilter} \nEned at:`)
+                       .setTimestamp()
+					 m.edit('** 🎉 GIVEAWAY ENDED 🎉**' , {embed: endEmbed});
+					message.guild.channels.get(room).send(`**Congratulations ${gFilter}! You won The \`${title}\`**` , {embed: {}})
+                     },duration);
+                   });
+                } catch(e) {
+                message.channel.send(`:heavy_multiplication_x:| **i Don't Have Prem**`);
+                  console.log(e);
+                }
+              });
+            });
+          });
+        });
+      });
+    });
+  }
+});
 
 client.on('message', message => {
     if(message.content.startsWith('$help')) {
