bd2db3559d712c437f6e5499b3a03bb26d17366e
cdnkvdbjjkdbnkzsdsdkdhsdsjdshod <41491967+cdnkvdbjjkdbnkzsdsdkdhsdsjdshod@users.noreply.github.com>
2018-07-26 23:22:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -429,7 +429,7 @@ hours = 12;
 }
 
   var filter = m => m.author.id === message.author.id;
-  if(message.content.startsWith(prefix + "$giveaway")) {
+  if(message.content.startsWith(prefix + "giveaway")) {
      //return message.channel.send(':heavy_multiplication_x:| **هذا الامر معطل حاليا.. ``حاول في وقت لاحق``**');
     if(!message.guild.member(message.author).hasPermission('MANAGE_GUILD')) return message.channel.send(':heavy_multiplication_x:| **يجب أن يكون لديك خاصية التعديل على السيرفر**');
     message.channel.send(`:eight_pointed_black_star:| **Send ID channel For the Giveaway**`).then(msg => {
@@ -465,7 +465,7 @@ hours = 12;
                   let giveEmbed = new Discord.RichEmbed()
                   .setDescription(`**${title}** \nReact With 🎉 To Enter! \n**Time remaining:${duration / 60000} Minutes**\n **Created at :** ${hours}:${minutes}:${seconds} ${suffix}`)
                   .setFooter(message.author.username, message.author.avatarURL);
-                  message.guild.channels.find(room).send(' :heavy_check_mark: **Giveaway Created** :heavy_check_mark:' , {embed: giveEmbed}).then(m => {
+                  message.guild.channel(room).send(' :heavy_check_mark: **Giveaway Created** :heavy_check_mark:' , {embed: giveEmbed}).then(m => {
                      let re = m.react('🎉');
                      setTimeout(() => {
                        let users = m.reactions.get("🎉").users;
@@ -477,7 +477,7 @@ hours = 12;
                        .addField('Giveaway Ended !🎉',`Winners : ${gFilter} \nEneded at:`)
                        .setTimestamp()
 					 m.edit('** 🎉 GIVEAWAY ENDED 🎉**' , {embed: endEmbed});
-					message.guild.channels.find(room).send(`**Congratulations ${gFilter}! You won The \`${title}\`**` , {embed: {}})
+					message.guild.channel(room).send(`**Congratulations ${gFilter}! You won The \`${title}\`**` , {embed: {}})
                      },duration);
                    });
                 } catch(e) {
